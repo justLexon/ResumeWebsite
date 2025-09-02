7bd0bca87b4ce57723784ad2a3312f6a25659e75
Lexon Schexnider <otherslexon@gmail.com>
2025-09-01 21:36:14 -0500

    Added Profile Quick Info, Updated ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ***Stylized Resume Website For Myself***
+
+
+With TS:
+-Profile Sidebar should be optional. Arrow to enable/disabled sidebar view. Make width for the sidebar in page.tsx 0 and the line divider also 0.
--- frontend/resumewebsite/src/sections/profilesidebar/sidebarInfo.tsx
+++ frontend/resumewebsite/src/sections/profilesidebar/sidebarInfo.tsx
@@ -6,8 +6,18 @@ export default function SidebarInfo() {
       {/* Profile Image */}
         <SidebarImg />
 
-      {/* Name */}
-        <h1 className="flex justify-center text-xl sm:text-1xl md:text-2xl lg:text-2xl">Lexon Schexnider</h1>
+      {/* Quick Info */}
+      <div className="flex flex-col justify-center m-3 gap-5 text-xl sm:text-xl md:text-2xl lg:text-2xl">
+        <h1>Lexon Schexnider</h1>
+        <p>I am an aspiring software engineer with a focus on cloud computing.</p>
+
+        <p className="text-base">OthersLexon@gmail.com</p>
+        <p className="text-base">Lafayette, LA</p>
+
+        <p className="text-sm">University of Louisiana at Lafayette</p>
+
+
+      </div>
 
     </div>
   );
